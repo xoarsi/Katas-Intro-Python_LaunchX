1c78a497dc63d92b34e1f0a9ec1da0440bbd180e
Sagrario Xochipitzahuac Arciniega Silva <xoarci@gmail.com>
2022-02-17 14:17:33 -0600

    Agregué las katas del módulo 10.

---
+++ config.py
@@ -0,0 +1,59 @@
+#Try y Except
+def main():
+    try:
+        configuration = open('config.txt')
+    except FileNotFoundError:
+        print("Couldn't find the config.txt file!")
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+def main():
+    try:
+        configuration = open('config.py')
+    except FileNotFoundError:
+        print("Couldn't find the config.py file!")
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+#Probando main()
+def main():
+    try:
+        configuration = open('config.txt')
+    except Exception:
+        print("Couldn't find the config.txt file!")
+
+def main():
+    try:
+        configuration = open('config.txt')
+    except FileNotFoundError:
+        print("Couldn't find the config.txt file!")
+    except IsADirectoryError:
+        print("Found config.txt but it is a directory, couldn't read it")
+
+def main():
+    try:
+        configuration = open('config.txt')
+    except FileNotFoundError:
+        print("Couldn't find the config.txt file!")
+    except IsADirectoryError:
+        print("Found config.txt but it is a directory, couldn't read it")
+    except (BlockingIOError, TimeoutError):
+        print("Filesystem under heavy load, can't complete reading configuration file")
+        
+def main():
+    try:
+        configuration = open('config.txt')
+    except FileNotFoundError:
+        print("Couldn't find the config.txt file!")
+    except IsADirectoryError:
+        print("Found config.txt but it is a directory, couldn't read it")
+    except (BlockingIOError, TimeoutError):
+        print("Filesystem under heavy load, can't complete reading configuration file")
